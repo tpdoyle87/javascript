03929375d2628ae325e23725ea00969bf2ada7fd
Thomas Doyle-Engler <tpdoyle87@gmail.com>
2018-06-17 21:30:25 -0400

    adding the second part about scope

---
+++ basics/scope-2.js
@@ -0,0 +1,18 @@
+//Global ()
+  //Local ()
+    //Local
+  //Local
+
+//let name = 'Thomas';
+
+if (true) {
+  //let name = 'Mike';
+  if (true) {
+    let name = 'Jen';
+    console.log(name);
+  }
+}
+
+if (true) {
+ console.log(name);
+}
