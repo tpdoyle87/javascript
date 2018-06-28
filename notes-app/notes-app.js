0cff1b9609a002877559ad5a26e575c54f2f206b
Thomas Doyle-Engler <tpdoyle87@gmail.com>
2018-06-28 18:32:26 -0300

    Playing with the DOM

---
+++ notes-app/notes-app.js
@@ -0,0 +1,13 @@
+// DOM - Document Object Model
+
+// Query and remove
+// const p = document.querySelector('p')
+// p.remove()
+
+// Query all and remove
+const ps = document.querySelectorAll('p')
+ps.forEach(function(p) {
+p.textContent = '*****************'
+//  console.log(p.textContent)
+//  p.remove()
+})
